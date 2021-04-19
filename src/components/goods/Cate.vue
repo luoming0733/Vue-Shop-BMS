6e6637caf29946d74958eb56082deb720ed1d04e
lomi-luo <luoming0733@126.com>
2021-04-20 04:04:27 +0800

    feat

--- src/components/goods/Cate.vue
+++ src/components/goods/Cate.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>商品分类</div>
+  <div>
+    <!-- 面包屑导航区域 -->
+    <el-breadcrumb separator="/">
+      <el-breadcrumb-item :to="{ path: '/home' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>商品分类</el-breadcrumb-item>
+    </el-breadcrumb>
+
+    <!-- 卡片视图 -->
+    <el-card> </el-card>
+  </div>
 </template>
 
 <script>
